c753412d5550a4ccd98c4dc541d35fec9532977c
lonewolf03103 <122261701+lonewolf03103@users.noreply.github.com>
2023-01-10 22:34:29 -0500

    Update salvation.c

--- salvation.c
+++ salvation.c
@@ -53,7 +53,7 @@ int ResponsesPerKeyword[NUMKEYWORDS]= {
 	3,2,4,5,5
 	
 	};
-const char *responses[NUMKEYWORDS][550] = {
+const char *responses[NUMKEYWORDS][150] = {
     		{"YES","NO","MAYBE*","SO","WHY"},
 {"AND THEN","HAVE FAITH*","BE FAST","MOVE SLOW","I WILL DO IT"},
 {"SOME","ALL","NONE","POOH*","GOOD AMOUNT"},
