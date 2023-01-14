065c0249fe8a068bd29b9ba57da59d5b8b947f7c
lonewolf03103 <122261701+lonewolf03103@users.noreply.github.com>
2023-01-14 07:55:30 -0700

    Update Eliza.c

--- Eliza.c
+++ Eliza.c
@@ -1,24 +1,31 @@
 
+/* eliza.c  
+ * ys
+ * original code by Weizenbaum, 1966
+ * this rendition based on Charles Hayden's Java implementation from http://chayden.net/eliza/Eliza.html
+ *
+ * Note: There are certainly far more optimal and elegant ways to code this... we kept this
+ * structure to be faithful to the original.  -scaz 
+ */
+
+#include <ctype.h>
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h>
 
-
-#define NUMKEYWORDS 30
-#define MAXLINELEN  16
+#define NUMKEYWORDS 15
+#define MAXLINELEN 40
 #define NUMSWAPS 230
 
-const char *keywords[]={
-"YES","NO","MAYBE*","SO","WHY",
+const char *keywords[]= {
+       "YES","NO","MAYBE*","SO","WHY",
   "KISS","DO","WILL","ARE","IS",
-  "THIS","THAT","THEY","YOU","I",
-    "AM","HAVE","THE","ON","ME",
-  	"LOOK","MUST","WITH","THOSE","IT",
-  	"NOT","POOH","GOOD","AND","OKAY","WHY"
+  "THIS","THAT","THEY","YOU","I"
 			
-  };
-const char *SWAPS[NUMSWAPS][42] = {
+};
 
+const char *SWAPS[NUMSWAPS][5] = {
+  
+  
 "YES","NO","MAYBE*","SO","WHY",
 "AND THEN","HAVE FAITH*","BE FAST","MOVE SLOW","I WILL DO IT",
 "SOME","ALL","NONE","POOH*","GOOD AMOUNT",
@@ -65,132 +72,300 @@ const char *SWAPS[NUMSWAPS][42] = {
 "OH MY...","TEA","COOKIES","WHAT MILK","THOSE DOGS","OH YOU MEAN MY CATS",  
 "FIND OUT THAT","DO THAT","CANDLES","CANDLE WAX","SPECIAL EDITION",
 "BE INDIRECT","BE CREATIVE","BE LOVE AND I AM THE HOT AIR THAT GOES WITH IT","GET TO KNOW MY OVEN!!!*","PLEASE SIT DOWN"
+
+  
+  
 };
 
-int ResponsesPerKeyword[NUMKEYWORDS][5]= {
-8,8,8,8,4,
-5,5,4,5,4,
-4,6,5,3,2,
-5,6,7,8,4,
-4,5,6,3,4,
-6,7,5,4,2,
-2,3,4,6,4,
-4,2,3,4,1,
-8,7,6,4,5,
-5,6,4,3,5,
-6,7,4,1,1,
-4,5,6,4,3,
-2,3,4,5,6,
-4,5,3,4,2,
-8,8,8,8,4,
-5,5,4,5,4,
-4,6,5,3,2,
-5,6,7,8,4,
-4,5,6,3,4,
-6,7,5,4,2,
-2,3,4,6,4,
-4,2,3,4,1,
-8,7,6,4,5,
-5,6,4,3,5,
-6,7,4,1,1,
-4,5,6,4,3,
-2,3,4,5,6,
-4,5,3,4,2,
-8,8,8,8,4,
-5,5,4,5,4,
-4,6,5,3,2,
-5,6,7,8,4,
-4,5,6,3,4,
-6,7,5,4,2,
-2,3,4,6,4,
-4,2,3,4,1,
-8,7,6,4,5,
-5,6,4,3,5,
-6,7,4,1,1,
-4,5,6,4,3,
-2,3,4,5,6,
-4,5,3,4,2,
-8,8,8,8,4,
-5,5,4,5,4,
-4,6,5,3,2,
-5,6,7,8,4,
-4,5,6,3,4,
-6,7,5,4,2,
-2,3,4,6,4,
-4,2,3,4,1,
-8,7,6,4,5,
-5,6,4,3,5,
-6,7,4,1,1,
-4,5,6,4,3,
-2,3,4,5,6,
-4,5,3,4,2,
-8,8,8,8,4,
-5,5,4,5,4,
-4,6,5,3,2,
-5,6,7,8,4,
-4,5,6,3,4,
-6,7,5,4,2,
-2,3,4,6,4,
-4,2,3,4,1,
-8,7,6,4,5,
-5,6,4,3,5,
-6,7,4,1,1,
-4,5,6,4,3,
-2,3,4,5,6,
-4,5,3,4,2,
-8,8,8,8,4,
-5,5,4,5,4,
-4,6,5,3,2,
-5,6,7,8,4,
-4,5,6,3,4,
-6,7,5,4,2,
-2,3,4,6,4,
-4,2,3,4,1,
-8,7,6,4,5,
-5,6,4,3,5,
-6,7,4,1,1,
-4,5,6,4,3,
-2,3,4,5,6,
-4,5,3,4,2,
-8,8,8,8,4,
-5,5,4,5,4,
-4,6,5,3,2,
-5,6,7,8,4,
-4,5,6,3,4,
-6,7,5,4,2,
-2,3,4,6,4,
-4,2,3,4,1,
-8,7,6,4,5,
-5,6,4,3,5,
-6,7,4,1,1,
-4,5,6,4,3,
-2,3,4,5,6,
-4,5,3,4,2,
-8,8,8,8,4,
-5,5,4,5,4,
-4,6,5,3,2,
-5,6,7,8,4,
-4,5,6,3,4,
-6,7,5,4,2,
-2,3,4,6,4,
-4,2,3,4,1,
-8,7,6,4,5,
-5,6,4,3,5,
-6,7,4,1,1,
-4,5,6,4,3,
-2,3,4,5,6,
-4,5,3,4,2,
+int ResponsesPerKeyword[NUMKEYWORDS][230]= {
+
+			4,4,3,2,1,
+			1,2,4,3,2,
+			1,1,3,2,2,
+			4,3,2,1,1,
+			6,5,3,3,2,
+			
+			4,4,3,2,1,
+			1,2,4,3,2,
+			1,1,3,2,2,
+			4,3,2,1,1,
+			6,5,3,3,2,
+			
+			4,4,3,2,1,
+			1,2,4,3,2,
+			1,1,3,2,2,
+			4,3,2,1,1,
+			6,5,3,3,2,
+			
+			4,4,3,2,1,
+			1,2,4,3,2,
+			1,1,3,2,2,
+			4,3,2,1,1,
+			6,5,3,3,2,
+			
+			4,4,3,2,1,
+			1,2,4,3,2,
+			1,1,3,2,2,
+			4,3,2,1,1,
+			6,5,3,3,2,
+			
+			4,4,3,2,1,
+			1,2,4,3,2,
+			1,1,3,2,2,
+			4,3,2,1,1,
+			6,5,3,3,2,
+			
+			4,4,3,2,1,
+			1,2,4,3,2,
+			1,1,3,2,2,
+			4,3,2,1,1,
+			6,5,3,3,2,
+			
+			4,4,3,2,1,
+			1,2,4,3,2,
+			1,1,3,2,2,
+			4,3,2,1,1,
+			6,5,3,3,2,
+			
+			4,4,3,2,1,
+			1,2,4,3,2,
+			1,1,3,2,2,
+			4,3,2,1,1,
+			6,5,3,3,2,
+			
+			4,4,3,2,1,
+			1,2,4,3,2,
+			1,1,3,2,2,
+			4,3,2,1,1,
+			6,5,3,3,2,
+			
+			4,4,3,2,1,
+			1,2,4,3,2,
+			1,1,3,2,2,
+			4,3,2,1,1,
+			6,5,3,3,2,
+			
+			4,4,3,2,1,
+			1,2,4,3,2,
+			1,1,3,2,2,
+			4,3,2,1,1,
+			6,5,3,3,2,
+			
+			4,4,3,2,1,
+			1,2,4,3,2,
+			1,1,3,2,2,
+			4,3,2,1,1,
+			6,5,3,3,2,
+			
+			4,4,3,2,1,
+			1,2,4,3,2,
+			1,1,3,2,2,
+			4,3,2,1,1,
+			6,5,3,3,2,
+			
+			4,4,3,2,1,
+			1,2,4,3,2,
+			1,1,3,2,2,
+			4,3,2,1,1,
+			6,5,3,3,2,
+			
+			4,4,3,2,1,
+			1,2,4,3,2,
+			1,1,3,2,2,
+			4,3,2,1,1,
+			6,5,3,3,2,
+			
+			4,4,3,2,1,
+			1,2,4,3,2,
+			1,1,3,2,2,
+			4,3,2,1,1,
+			6,5,3,3,2,
+			
+			4,4,3,2,1,
+			1,2,4,3,2,
+			1,1,3,2,2,
+			4,3,2,1,1,
+			6,5,3,3,2,
+			
+			4,4,3,2,1,
+			1,2,4,3,2,
+			1,1,3,2,2,
+			4,3,2,1,1,
+			6,5,3,3,2,
+			
+			4,4,3,2,1,
+			1,2,4,3,2,
+			1,1,3,2,2,
+			4,3,2,1,1,
+			6,5,3,3,2,
+			
+			4,4,3,2,1,
+			1,2,4,3,2,
+			1,1,3,2,2,
+			4,3,2,1,1,
+			6,5,3,3,2,
+			
+			4,4,3,2,1,
+			1,2,4,3,2,
+			1,1,3,2,2,
+			4,3,2,1,1,
+			6,5,3,3,2,
+			
+			4,4,3,2,1,
+			1,2,4,3,2,
+			1,1,3,2,2,
+			4,3,2,1,1,
+			6,5,3,3,2,
+			
+			4,4,3,2,1,
+			1,2,4,3,2,
+			1,1,3,2,2,
+			4,3,2,1,1,
+			6,5,3,3,2,
+			
+			
+			4,4,3,2,1,
+			1,2,4,3,2,
+			1,1,3,2,2,
+			4,3,2,1,1,
+			6,5,3,3,2,
+			
+			4,4,3,2,1,
+			1,2,4,3,2,
+			1,1,3,2,2,
+			4,3,2,1,1,
+			6,5,3,3,2,
+			
+			4,4,3,2,1,
+			1,2,4,3,2,
+			1,1,3,2,2,
+			4,3,2,1,1,
+			6,5,3,3,2,
+			
+			4,4,3,2,1,
+			1,2,4,3,2,
+			1,1,3,2,2,
+			4,3,2,1,1,
+			6,5,3,3,2,
+			
+			4,4,3,2,1,
+			1,2,4,3,2,
+			1,1,3,2,2,
+			4,3,2,1,1,
+			6,5,3,3,2,
+			
+			4,4,3,2,1,
+			1,2,4,3,2,
+			1,1,3,2,2,
+			4,3,2,1,1,
+			6,5,3,3,2,
+			
+			4,4,3,2,1,
+			1,2,4,3,2,
+			1,1,3,2,2,
+			4,3,2,1,1,
+			6,5,3,3,2,
+			
+			4,4,3,2,1,
+			1,2,4,3,2,
+			1,1,3,2,2,
+			4,3,2,1,1,
+			6,5,3,3,2,
+			
+			4,4,3,2,1,
+			1,2,4,3,2,
+			1,1,3,2,2,
+			4,3,2,1,1,
+			6,5,3,3,2,
+			
+			4,4,3,2,1,
+			1,2,4,3,2,
+			1,1,3,2,2,
+			4,3,2,1,1,
+			6,5,3,3,2,
+			
+			4,4,3,2,1,
+			1,2,4,3,2,
+			1,1,3,2,2,
+			4,3,2,1,1,
+			6,5,3,3,2,
+			
+			4,4,3,2,1,
+			1,2,4,3,2,
+			1,1,3,2,2,
+			4,3,2,1,1,
+			6,5,3,3,2,
+			
+			4,4,3,2,1,
+			1,2,4,3,2,
+			1,1,3,2,2,
+			4,3,2,1,1,
+			6,5,3,3,2,
+			
+			4,4,3,2,1,
+			1,2,4,3,2,
+			1,1,3,2,2,
+			4,3,2,1,1,
+			6,5,3,3,2,
+			
+			4,4,3,2,1,
+			1,2,4,3,2,
+			1,1,3,2,2,
+			4,3,2,1,1,
+			6,5,3,3,2,
+			
+			4,4,3,2,1,
+			1,2,4,3,2,
+			1,1,3,2,2,
+			4,3,2,1,1,
+			6,5,3,3,2,
+			
+			4,4,3,2,1,
+			1,2,4,3,2,
+			1,1,3,2,2,
+			4,3,2,1,1,
+			6,5,3,3,2,
+			
+			4,4,3,2,1,
+			1,2,4,3,2,
+			1,1,3,2,2,
+			4,3,2,1,1,
+			6,5,3,3,2,
+			
+			4,4,3,2,1,
+			1,2,4,3,2,
+			1,1,3,2,2,
+			4,3,2,1,1,
+			6,5,3,3,2,
+			
+			4,4,3,2,1,
+			1,2,4,3,2,
+			1,1,3,2,2,
+			4,3,2,1,1,
+			6,5,3,3,2,
+			
+			4,4,3,2,1,
+			1,2,4,3,2,
+			1,1,3,2,2,
+			4,3,2,1,1,
+			6,5,3,3,2,
+			
+			4,4,3,2,1,
+			1,2,4,3,2,
+			1,1,3,2,2,
+			4,3,2,1,1,
+			6,5,3,3,2
+
+};
+           
+const char *responses[NUMKEYWORDS][5] = { 
 
 
-	};
-	
-const char *responses[NUMKEYWORDS][5] = {
- 
-"YES","NO","MAYBE*","SO","WHY",
-"AND THEN","HAVE FAITH*","BE FAST","MOVE SLOW","I WILL DO IT",
-"SOME","ALL","NONE","POOH*","GOOD AMOUNT",
-"NO TRY","WHY TRY","CARROT*","I KNOW THINGS","I BELIEVE SOMETHING",
-"WHAT DO YOU BELIEVE","WHAT DO WE BELIEVE*","BE ABSTRACT","BE DIRECT",
+ "YES","NO","MAYBE*","SO","WHY",
+  "AND THEN","HAVE FAITH*","BE FAST","MOVE SLOW","I WILL DO IT",
+   "SOME","ALL","NONE","POOH*","GOOD AMOUNT",
+    "NO TRY","WHY TRY","CARROT*","I KNOW THINGS","I BELIEVE SOMETHING",
+     "WHAT DO YOU BELIEVE","WHAT DO WE BELIEVE*","BE ABSTRACT","BE DIRECT",
 "FANCY THAT","NO WAY","I WILL CONSIDER THAT AGAIN*","YOU GET EYES","I HAVE NICE EYES",
 "I LIKE YOUR EYES*","WHAT DO YOUR EYES LOOK LIKE","NOW THAT IS A BAD THEORY","WHAT WE REALLY NEED ARE PROGRESS REPORTS","NO I AM NOT A POLITICIAN",
 "NOW THAT IS AN IDEA*","I WANT INFORMATION","INFORM ME AGAIN ABOUT WHY YOURE CRAZY","CRAZY? NO I AM NOT CRAZY","YOURE CRAZY",
@@ -232,8 +407,11 @@ const char *responses[NUMKEYWORDS][5] = {
 "OH MY...","TEA","COOKIES","WHAT MILK","THOSE DOGS","OH YOU MEAN MY CATS",  
 "FIND OUT THAT","DO THAT","CANDLES","CANDLE WAX","SPECIAL EDITION",
 "BE INDIRECT","BE CREATIVE","BE LOVE AND I AM THE HOT AIR THAT GOES WITH IT","GET TO KNOW MY OVEN!!!*","PLEASE SIT DOWN"
-};
 
+
+        
+    
+};
 
 void print_center(const char *msg) {
   int numspaces=(MAXLINELEN-strlen(msg))/2;
@@ -246,53 +424,74 @@ void print_center(const char *msg) {
 
 void print_title () {
     printf("\n\n");
-    printf("YOU ARE DETECTED");
-	printf("\n");
+    print_center("*** ELIZA ***");
+    print_center("Original code by Weizenbaum, 1966");
+    print_center("To stop Eliza, type 'bye'");
+    printf("\n\n");
+    printf("HI!  I'M ELIZA.  WHAT'S YOUR PROBLEM?\n");
 }
 
 void readline(char *instr) {
-
     char c;
     int slen=0;
 
     c=getchar();
     while (c != '\n')
     {
-
+        // removes punctuation and sets to uppercase
         if(isalpha(c) || isspace(c))
             instr[slen++]=toupper(c);
         if(slen>MAXLINELEN-1)
-	{
-	break;
+        {
+          printf("Exceeded Max Line Length\n");
         }
         c=getchar();
     } 
     instr[slen]='\0';
 }
 
-int main()
+
+void main()
 {
   int k,baseLength; 
   int whichReply[NUMKEYWORDS];
   char lastinput[MAXLINELEN];
   char reply[MAXLINELEN];
   char *baseResponse, *token;
-  const char separator[1]=" ";
+  const char separator[2]=" ";
   char inputstr[MAXLINELEN];
   int x,s;
   char *location;
 
+  // use the first reply for each keyword match the first time you see that keyword
   for (x=0;x<NUMKEYWORDS; x++) {
     whichReply[x] = 0;
-}
+  }
 
+  // print a nice centered title screen
   print_title();
-  lastinput[x]='\0';
+
+  lastinput[0]='\0';
+
   while (1) { 
-    printf(":");
+    printf(">");
     readline(inputstr);
     printf("\n");
+
+    // check for termination 
+    if (strcmp(inputstr,"BYE")==0)
+      break; 
+
+    // check for repeated entries 
+    if (strcmp(lastinput,inputstr)==0) 
+    {
+      printf("PLEASE DON'T REPEAT YOURSELF!\n");
+      continue;
+    }
     strncpy(lastinput,inputstr,strlen(inputstr)+1); 
+
+    // see if any of the keywords is contained in the input 
+    // if not, we use the last element of keywords as our default responses 
     strcpy(reply,"");
     for(k=0;k<NUMKEYWORDS-1;k++)
     {
@@ -301,16 +500,26 @@ int main()
         break;
     }
 
+    // Build Eliza's response 
+    // start with Eliza's canned response, based on the keyword match
     baseResponse = (char *) responses[k][whichReply[k]];
     baseLength = strlen(baseResponse);
+
     if(baseResponse[baseLength-1] != '*')
     {
+      // if we have a baseResponse without an asterix, just use it as-is
       strcat(reply, baseResponse);
     }
     else
     {
+      // if we do have an asterix, fill in the remaining with the user input
+      // use all but the last character of the base response
       strncat(reply, baseResponse, baseLength-1);
+
+      // now add in the rest of the user's input, starting at <location>
+      // but skip over the keyword itself
       location+=strlen(keywords[k]);
+      // take them one word at a time, so that we can substitute pronouns
       token = strtok(location, separator);
       while(token != NULL)
       {
@@ -318,15 +527,10 @@ int main()
         {   
           if(strcmp(SWAPS[s][0], token) == 0)
           {
-            token = (char *) SWAPS[s][x];
-            break;          
-          }          
-			if(strcmp(SWAPS[x][1], token) == 1)
-			{
-				token = (char *) SWAPS[s][k];
-				break;
-		 }      
-}   
+            token = (char *) SWAPS[s][1];
+            break;
+          }
+        }   
         strcat(reply," ");
         strcat(reply, token);
         token=strtok(NULL, separator);
@@ -335,13 +539,10 @@ int main()
     }
     printf("%s\n", reply);
 
+    // next time, use the next appropriate reply for that keyword
     whichReply[k]++;
     if ( whichReply[k] >= *ResponsesPerKeyword[k])
-      whichReply[k] = *ResponsesPerKeyword[s];
-	else
-	whichReply[k] = *ResponsesPerKeyword[k];
-	continue;	
+      whichReply[k] = 0;
   } 
-  printf( "\n");
-  return(*baseResponse);
+  printf( "GOODBYE!  THANKS FOR VISITING WITH ME...\n");
 }
